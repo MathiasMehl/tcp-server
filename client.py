771551620eaa7cb1bb51c0f0e844ffdf9cc72f37
klarparatstart <kristinalarsenova@gmail.com>
2020-10-18 13:08:24 +0200

    [refactor] add removed send call in http_request and refactor

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 import socket
 import sys
 import threading
+import time
 
 PRINT_LOCK = threading.Lock()
 HOST = "127.0.0.1"
 PORT = 8002
+HTTP_requests = ["GET", "TRACE", "OPTIONS", "HEAD", "PUT", "POST", "DELETE"]
 
 
 def print_with_lock(arg):
@@ -12,19 +14,14 @@ def print_with_lock(arg):
         print(f"[{threading.current_thread().getName()}]" + arg)
 
 
-def connect_server(host):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, PORT))
+def connect_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((HOST, PORT))
 
-    return s
+    return server
 
 
-def send_HTTP_request(host, url, method):
-    server = connect_server(host)
-
-    request = f"{method} {url} HTTP/1.1\r\n\r\n"
-
-    print_with_lock(f" Attempting '{method}' request to host '{host} on url '{url}'")
+def send_and_receive_data(server, request):
     server.sendall(request.encode("UTF-8"))
 
     data = server.recv(1024).decode("UTF-8")
@@ -33,20 +30,44 @@ def send_HTTP_request(host, url, method):
 
     response_body = "\n".join(data.split("\n")[1:]).strip()
 
-    print_with_lock(f" Response from '{method}' request to '{host}' on '{url}' had metadata: {http_meta}")
-    print_with_lock(f" Body received from '{method}' request: \n {response_body} ")
+    return http_meta, response_body
+
+
+def ping_server(server):
+    start_time = time.time()
+    send_and_receive_data(server, "ping")
+    elapsed_time = time.time() - start_time
+    print(f"pinged {server.getpeername()} | ping : {elapsed_time}")
+
+
+def client(method, url):
+    server = connect_server()
+
+    if method in HTTP_requests:
+        http_request(server, url, method)
+    elif method == "ping":
+        ping_server(server)
 
     server.close()
 
 
+def http_request(server, url, method):
+    request = f"{method} {url} HTTP/1.1\r\n\r\n"
+
+    http_meta, response_body = send_and_receive_data(server, request)
+
+    print_with_lock(f" Attempting '{method}' request to host '{HOST} on url '{url}'")
+
+    send_and_receive_data(server, request)
+
+    print_with_lock(f" Response from '{method}' request to '{HOST}' on '{url}' had metadata: {http_meta}")
+    print_with_lock(f" Body received from '{method}' request: \n {response_body} ")
+
+
 if 1 < sys.argv.__len__():
     method = sys.argv[1]
-    host = sys.argv[2]
-    url = sys.argv[3]
-    number_of_clients = int(sys.argv[4])
+    url = sys.argv[2]
+    number_of_clients = int(sys.argv[3])
 
-    HTTP_requests = ["GET", "TRACE", "OPTIONS", "HEAD", "PUT", "POST", "DELETE"]
-
-    if method in HTTP_requests:
-        for i in range(number_of_clients):
-            threading.Thread(target=send_HTTP_request, name=f"Client{i}", args=(host, url, method)).start()
+    for i in range(number_of_clients):
+        threading.Thread(target=client, name=f"Client{i}", args=(method, url)).start()
